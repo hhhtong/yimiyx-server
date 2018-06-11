8bce4367077ffb75ad0faa5e22b3321879d6f616
songyazhao <syazhao@foxmail.com>
2018-06-11 19:47:56 +0800

    update:version

--- app/web/page/purchase-manage/enquiry-list.vue
+++ app/web/page/purchase-manage/enquiry-list.vue
@@ -1,13 +1,13 @@
 <style lang="stylus">
 </style>
 
-<template lang="pug">
-  Layout(:style="{height: '100%'}" class="avf")
-    Layout(:style="{height: '100%'}")
+<template>
+  <Layout :style="{height: '100%'}"></Layout>
+    <!-- Layout(:style="{height: '100%'}")
       Content(:style="{margin: '0 20px 88px', background: '#fff', height: '100%'}")
         | Content
       //- Footer(:style="{position: 'fixed', bottom: 0, width: '100%'}") Footer
-    //- Sider(reverse-arrow collapsible) Sider
+    //- Sider(reverse-arrow collapsible) Sider --> -->
 </template>
 
 <script>
