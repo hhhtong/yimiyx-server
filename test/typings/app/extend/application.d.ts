44cef73f8307e77a50500d6fd95cec20f4906617
糖醋里脊叉烧包 <Zero@XdeMacBook-Pro.local>
2018-08-06 19:40:38 +0800

    [fix]:favicon.ico

--- test/typings/app/extend/application.d.ts
+++
@@ -1,9 +0,0 @@
-// This file was auto created by egg-ts-helper
-// Do not modify this file!!!!!!!!!
-
-import ExtendObject from '../../../app/extend/application';
-declare module 'egg' {
-  interface Application {
-    generateQRCode: typeof ExtendObject.generateQRCode;
-  }
-}
